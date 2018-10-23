5586a9ea93508581c39946ac70d7b4896c6f8f90
dmistera2 <dmistera2@gmail.com>
2018-10-23 21:50:22 +0200

    autorefresh

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,13 @@ client.on('message', (msg) => {
 })
 
 client.login("MjcyMDgzMzc4Mjc3MTg3NTg1.Dq-5Zg.nAfAj7bRS8NNhRR-8KXeOKlcKmQ");
+
+function keepAppUp() {
+    const http = require('http');
+    const express = require('express');
+    const app = express();
+    app.listen(8080);
+    setInterval(() => {
+        http.get('http://exile-discord.glitch.me/');
+    }, 8000);
+}
